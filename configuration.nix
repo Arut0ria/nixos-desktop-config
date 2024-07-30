d6a52059cf1d142ed28bd3d2103d0252d78c98c4
Arut0ria <anoxing64@gmail.com>
2024-07-30 20:35:26 +0200

    Started to add home manager

--- configuration.nix
+++ configuration.nix
@@ -90,6 +90,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     kitty
+    home-manager
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- flake.nix
+++ flake.nix
@@ -1,17 +1,36 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    home-mananger = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    let
       system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        inputs.disko.nixosModules.disko
-      ];
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          inputs.disko.nixosModules.disko
+        ];
+      };
+
+      #homeConfigurations.theo = home-manager.lib.homeManagerConfiguration {
+      #  inherit pkgs;
+      #  modules = [
+      #    ./home.nix
+      #  ];
+      };
     };
   };
 }
