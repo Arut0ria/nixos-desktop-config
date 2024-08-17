25b23adc5aa41878c263cd423deda937bd43c741
Arut0ria <anoxing64@gmail.com>
2024-08-17 21:48:29 +0200

    Option for working stylix and vscode

--- homeManagerModules/programs/vscode.nix
+++ homeManagerModules/programs/vscode.nix
@@ -1,11 +1,22 @@
 { pkgs, lib, config, ... }: {
   options = {
     vscode-program.enable = lib.mkEnableOption "Enables vscodium.";
+    /*
+      vscode-program.extraExtensions = lib.mkOption {
+      type = with lib.types; listOf package;
+      description = "List of additional VSCode extensions to install.";
+      default = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        bierner.emojisense
+      ];
+      };
+    */
   };
 
   config = lib.mkIf config.vscode-program.enable {
     programs.vscode = {
       enable = true;
+      mutableExtensionsDir = false;
       package = pkgs.vscodium;
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
