cec66bcbdb8936e5cb147cabede870a204b93dad
Arut0ria <anoxing64@gmail.com>
2024-08-28 21:40:20 +0200

    Zsh ctrl arrow fix

--- homeManagerModules/programs/zsh.nix
+++ homeManagerModules/programs/zsh.nix
@@ -11,6 +11,8 @@
       syntaxHighlighting.enable = true;
 
       initExtra = ''
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
         [ -f ./.p10k.zsh ] && source ./.p10k.zsh
         ${pkgs.fastfetch}/bin/fastfetch
       '';
