472e27eefaa6bc71e28edcff5a45313dc67f96a3
Arut0ria <anoxing64@gmail.com>
2024-10-06 09:37:22 +0200

    Cleaned laptop config + stylix background

--- nixosModules/programs/stylix.nix
+++ nixosModules/programs/stylix.nix
@@ -1,17 +1,43 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, config, ... }:
+let
+  background = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/Arut0ria/nixos-desktop-config/main/images/672183.jpg";
+    sha256 = "0g0z89miryai32w51q2is7p18pwg1mx6jcagjxbaiaamf35hx4wa";
+  };
+
+  # logo = pkgs.fetchurl {
+  #   # url = "https://raw.githubusercontent.com/Arut0ria/nixos-desktop-config/main/images/term_logo.jpg";
+  #   url = "https://raw.githubusercontent.com/Arut0ria/nixos-desktop-config/main/images/term_logo_2.jpg";
+  #   sha256 = "174bvzc9qgbjizzmx7lwdv7bl2bjlc0g0r4n2psd5hakjlglalma";
+  # };
+  logo = pkgs.stdenv.mkDerivation {
+    name = "sddm_logo";
+    src = "nixosModules/programs/custom_nix_logo.svg";
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out
+      cp custom_nix_logo.svg share/sddm/themes/breeze/theme.conf.user
+    '';
+  };
+in
+{
   options = {
     stylix-module.enable = lib.mkEnableOption "Enables stylix.";
   };
 
   config = lib.mkIf config.stylix-module.enable {
+    environment.systemPackages = [
+      (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
+        [General]
+        background=${background}
+      '')
+    ];
+
     # Enable stylix
     stylix = {
       enable = true;
       autoEnable = true;
-      image = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/Arut0ria/nixos-desktop-config/main/images/672183.jpg";
-        sha256 = "0g0z89miryai32w51q2is7p18pwg1mx6jcagjxbaiaamf35hx4wa";
-      };
+      image = background;
 
       polarity = "dark";
 
