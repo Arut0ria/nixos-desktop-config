f71a02c640aa85ca18bf39a90b28b57d1e5cf0b0
Arut0ria <anoxing64@gmail.com>
2024-12-03 17:19:11 +0100

    Addtional hyprland config, basic keymap and gaps.

--- homeManagerModules/hyprland/hyprland.nix
+++ homeManagerModules/hyprland/hyprland.nix
@@ -5,13 +5,24 @@
       enable = true;
       settings = {
         "$mod" = "SUPER";
+        
+        input = {
+          kb_layout = "fr";
+        };
+
+        general = {
+          gaps_in = 2;
+          gaps_out = 2;
+        };
 
         bindm = [
           # mouse movements
           "$mod, mouse:272, movewindow"
           "$mod, mouse:273, resizewindow"
           "$mod ALT, mouse:272, resizewindow"
-          "$mod T, exec, kitty"
+        ];
+        bind = [
+          "$mod, T, exec, kitty"
         ];
       };
     };
