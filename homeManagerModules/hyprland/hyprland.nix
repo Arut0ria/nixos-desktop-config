8606ad6c7202ddff356b3aff858365cbc4fc6edf
Arut0ria <anoxing64@gmail.com>
2024-12-10 16:53:58 +0100

    Modified hyprland config

--- homeManagerModules/hyprland/hyprland.nix
+++ homeManagerModules/hyprland/hyprland.nix
@@ -5,15 +5,30 @@
       enable = true;
       settings = {
         "$mod" = "SUPER";
-        
+
         input = {
           kb_layout = "fr";
+          touchpad = {
+            natural_scroll = true;
+          };
+        };
+
+        gestures = {
+          workspace_swipe = true;
         };
 
         general = {
           gaps_in = 2;
           gaps_out = 2;
         };
+
+        bezier = [
+          "ease-out-expo, 0.16, 1, 0.3, 1"
+        ];
+
+        animation = [
+          "workspaces, 1, 10, ease-out-expo, slide"
+        ];
 
         bindm = [
           # mouse movements
@@ -23,6 +38,23 @@
         ];
         bind = [
           "$mod, T, exec, kitty"
+          "$mod, F, exec, firefox"
+
+          "$mod CTRL, right, workspace, +1"
+          "$mod CTRL, left, workspace, -1"
+          
+          "$mod CTRL_SHIFT, right, movetoworkspace, +1"
+          "$mod CTRL_SHIFT, left, movetoworkspace, -1"
+        
+          "$mod ALT, left, movefocus, l"
+          "$mod ALT, down, movefocus, d"
+          "$mod ALT, up, movefocus, u"
+          "$mod ALT, right, movefocus, r"
+
+          "$mod ALT CTRL_ALT, left, movewindow, l"
+          "$mod ALT CTRL_ALT, down, movewindow, d"
+          "$mod ALT CTRL_ALT, up, movewindow, u"
+          "$mod ALT CTRL_ALT, right, movewindow, r"
         ];
       };
     };
